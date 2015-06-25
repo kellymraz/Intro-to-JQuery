59edfd698d9a983a77b92f20b4c811a54a8fa21d
Student <kitty8mars@gmail.com>
2015-06-25 17:34:02 -0600

    whatever you wanna write

---
+++ Intro-jQuery.js
@@ -0,0 +1,64 @@
+ // $( ".button" ).on( "click", function() {
+  // console.log( "Hey! You clicked on me!")
+
+// $( ".button" ).on( "click", function() {
+// 	$("body").append ("Please be nice to me today")
+
+// });
+
+// $( ".button" ).on( "click", function() {
+// 	$("h1").append ("Work vs. Play")
+
+// });
+
+ // $( ".button" ).on( "click", function() {
+	// $("ul").append ("coffee, tea, chocolate")
+ // });
+
+// }
+
+
+// console.log($(".button"))
+// you could add other things above and make a chain of methods
+// (ex. .text, .width, .val are all different methods)
+
+//END OF PART I
+
+// BEGINNING PART II
+
+$( "p" ).on( "mouseover", function() {
+	$("p").css("color", "pink");
+
+});
+
+$( "p" ).on( "mouseout", function() {
+	$("p").css("color", "black");
+
+});
+
+// $( "h1, h2, h3" ).append("!");
+
+// $("a").on("click", function(){
+// 	alert("Are you sure you want to navigate away from this page?")
+	
+
+
+// .append only takes variables that are strings
+// You can append straight text or html
+
+// If you made a div in html you could also append content inside of it, like text or additional html elements
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
