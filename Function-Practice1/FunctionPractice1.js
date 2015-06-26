00022e81fcad068818666803eb06741b3904eaa7
Student <kitty8mars@gmail.com>
2015-06-26 15:33:35 -0600

    Thank you Chris

---
+++ Function-Practice1/FunctionPractice1.js
@@ -0,0 +1,59 @@
+
+
+// // console.log("Working!")
+
+
+// var tripleFive =  
+// 	for (var i = 1; i <= 3; i++){
+// 	{
+// 	console.log("Five!");
+
+// 	}
+
+// }
+
+
+// This is a variable with an empty body
+// var tripleFive = _____;
+
+// // This is a function body with no logic
+
+// function(){
+
+// }
+
+
+// for (var i = 1;  i <= 3;  i++){}
+
+
+// console.log("Five!");
+
+// THINK OF THE RECIPE OF WORKING BACKWARDS
+// START WITH THE CAKE AND WORK BACKWARDS THROUGH THE RECIPE
+
+var tripleFive = function(){
+	for (var i = 1;  i <= 3;  i++){
+		console.log("Five!");
+
+	}
+};
+
+tripleFive()
+
+
+// This last bit calls the function so that it can be executed
+
+// We're saying that the console log will happen whenever the for-loop iterates, whenever the function() is called, by the name tripleFive
+
+
+// // #2
+// // Write a function called 'lastLetter' which takes a single string argument and returns the last character in the string
+// // Ex. lastLetter ('hello') should return "o"
+
+// // var lastLetter =  ()
+
+// // var newWord = prompt("Please enter a single word");
+// // 	console.log(newWord);
+
+// //  for (newWord)
+
